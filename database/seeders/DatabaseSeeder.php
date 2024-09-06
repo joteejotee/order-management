136536b625a3821de2d00130e4c8b9271cf53954
jo <jotee.jotee@gmail.com>
2024-09-06 23:05:51 +0900

    顧客テーブルの作成とダミーデータの挿入

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Customer;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            PenSeeder::class,//PenSeederの完全修飾名はDatabase\Seeders\PenSeeder
+            CustomerSeeder::class,
         ]);
     }
 }
