755767bcfb06fd8bf5fb8bc77cf9137ea113d4b2
jo <jotee.jotee@gmail.com>
2024-11-14 19:04:26 +0900

    @babel/coreの依存関係を追加、TSの型エラー修正

--- breeze_next_chikaraemon/src/app/(app)/Header.tsx
+++ breeze_next_chikaraemon/src/app/(app)/Header.tsx
@@ -1,4 +1,4 @@
-const Header = ({ title }) => {
+const Header = ({ title }: { title: string }) => {
     return (
         <header className="bg-white shadow">
             <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
