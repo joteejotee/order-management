1448017eaad4a11771c2a61624843ed9b55cc2ec
jo <jotee.jotee@gmail.com>
2024-11-18 19:45:04 +0900

    TSの型エラーの修正

--- breeze_next_chikaraemon/src/app/(app)/layout.tsx
+++ breeze_next_chikaraemon/src/app/(app)/layout.tsx
@@ -3,8 +3,9 @@
 import { useAuth } from '@/hooks/auth'
 import Navigation from '@/app/(app)/Navigation'
 import Loading from './Loading'
+import React from 'react' // Reactの型定義が必要
 
-const AppLayout = ({ children }) => {
+const AppLayout = ({ children }: { children: React.ReactNode }) => { // 型を明示的に指定
   const { user } = useAuth({ middleware: 'auth' })
 
   if (!user) {
