0b6ca1dece1e8dd41e8ac5f28844a89d858528cf
jo <jotee.jotee@gmail.com>
2024-11-19 20:17:11 +0900

    エラー修正

--- breeze_next_chikaraemon/src/components/Label.tsx
+++ breeze_next_chikaraemon/src/components/Label.tsx
@@ -1,4 +1,10 @@
-const Label = ({ className, children, ...props }) => (
+interface LabelProps {
+  className?: string // 必須ではなくオプションに変更
+  children: React.ReactNode
+  [x: string]: any // 他のプロパティも許可
+}
+
+const Label = ({ className = '', children, ...props }: LabelProps) => (
   <label
     className={`${className} block font-medium text-sm text-gray-700`}
     {...props}
