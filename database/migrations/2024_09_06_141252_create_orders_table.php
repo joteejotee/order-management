2092596602a83df612df2f79158a5b9923d7eb28
jo <jotee.jotee@gmail.com>
2024-11-24 22:48:35 +0900

    未出荷ボタンの表示エラーを修正

--- database/migrations/2024_09_06_141252_create_orders_table.php
+++ database/migrations/2024_09_06_141252_create_orders_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
       $table->foreignId('pen_id')->constrained();
       $table->integer('num');
       $table->datetime('orderday');
-      $table->string('shipping')->nullable(); // NULLを許可
+      $table->integer('shipping')->default(0);
       $table->timestamps();
     });
   }
