192ac46b840c38ce64ed2f2afcf7ede842a15e66
jo <jotee.jotee@gmail.com>
2025-02-18 19:35:39 +0900

    dashboard画面の表示修正

--- breeze_next_chikaraemon/src/app/(app)/dashboard/page.tsx
+++ breeze_next_chikaraemon/src/app/(app)/dashboard/page.tsx
@@ -1,11 +1,10 @@
 'use client';
 
 import { useAuth } from '@/hooks/auth';
-import AppLayout from '@/components/Layouts/AppLayout';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
-const DashboardPage = () => {
+const Dashboard = () => {
   const router = useRouter();
   const { user, isValidating } = useAuth({ middleware: 'auth' });
 
@@ -20,18 +19,14 @@ const DashboardPage = () => {
   }
 
   return (
-    <AppLayout>
-      <div className="py-12">
-        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div className="p-6 bg-white border-b border-gray-200">
-              ようこそ {user.name} さん！
-            </div>
-          </div>
+    <div className="py-12">
+      <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+          <div className="p-6 text-gray-900">ようこそ {user.name} さん！</div>
         </div>
       </div>
-    </AppLayout>
+    </div>
   );
 };
 
-export default DashboardPage;
+export default Dashboard;
