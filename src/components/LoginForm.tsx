092c4e2110ba991304ebf8f78c1f7e313b24fd5d
jo <jotee.jotee@gmail.com>
2025-03-06 19:39:20 +0900

    ログインエンドポイントを/api/loginから/loginに変更

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -19,7 +19,7 @@ const LoginForm = () => {
 
             // ログイン処理
             console.log("Sending login request");
-            const loginResponse = await axios.post("/api/login", {
+            const loginResponse = await axios.post("/login", {
                 email,
                 password,
                 remember: false,
