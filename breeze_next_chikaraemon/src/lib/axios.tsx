982074674c1d3d396e54ec33cbdddc2f5e42c483
jo <jotee.jotee@gmail.com>
2025-03-17 12:02:11 +0900

    APIのベースURLを環境変数から取得するように修正

--- breeze_next_chikaraemon/src/lib/axios.tsx
+++ breeze_next_chikaraemon/src/lib/axios.tsx
@@ -1,8 +1,7 @@
 import Axios from 'axios';
 
 const axios = Axios.create({
-  baseURL: 'http://localhost:8000',
-  // baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8000',
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
   },
