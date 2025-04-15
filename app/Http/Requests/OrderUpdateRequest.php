2e7a663b1ffb8315478dd5c5f5a37a0a91533931
jo <jotee.jotee@gmail.com>
2025-04-15 23:38:06 +0000

    “注文編集機能のエラー修正”

--- app/Http/Requests/OrderUpdateRequest.php
+++ app/Http/Requests/OrderUpdateRequest.php
@@ -17,6 +17,9 @@ class OrderUpdateRequest extends FormRequest
         return [
             'status' => ['sometimes', 'required', 'string', Rule::in(['pending', 'shipped'])],
             'shipping' => ['sometimes', 'required', 'boolean'],
+            'num' => ['sometimes', 'required', 'integer', 'min:1', 'max:20'],
+            'pen_id' => ['sometimes', 'required', 'integer', 'exists:pens,id'],
+            'customer_id' => ['sometimes', 'required', 'integer', 'exists:customers,id'],
         ];
     }
 
@@ -25,6 +28,9 @@ class OrderUpdateRequest extends FormRequest
         return [
             'status' => 'ステータス',
             'shipping' => '出荷状態',
+            'num' => '数量',
+            'pen_id' => '商品',
+            'customer_id' => '顧客',
         ];
     }
 
@@ -35,6 +41,16 @@ class OrderUpdateRequest extends FormRequest
             'status.in' => ':attributeの値が不正です。',
             'shipping.required' => ':attributeを指定してください。',
             'shipping.boolean' => ':attributeの形式が不正です。',
+            'num.required' => ':attributeを入力してください。',
+            'num.integer' => ':attributeは整数で入力してください。',
+            'num.min' => ':attributeは1以上で入力してください。',
+            'num.max' => ':attributeは20以下で入力してください。',
+            'pen_id.required' => ':attributeを選択してください。',
+            'pen_id.integer' => ':attributeは整数で入力してください。',
+            'pen_id.exists' => '選択された:attributeは存在しません。',
+            'customer_id.required' => ':attributeを選択してください。',
+            'customer_id.integer' => ':attributeは整数で入力してください。',
+            'customer_id.exists' => '選択された:attributeは存在しません。',
         ];
     }
 }
