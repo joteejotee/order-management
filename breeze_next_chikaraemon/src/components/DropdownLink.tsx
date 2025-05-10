c501ad183657457487c4fa85a1a68fd414b5e1c0
jo <jotee.jotee@gmail.com>
2025-05-10 15:53:24 +0900

    文字の大きく変更

--- breeze_next_chikaraemon/src/components/DropdownLink.tsx
+++ breeze_next_chikaraemon/src/components/DropdownLink.tsx
@@ -44,7 +44,7 @@ export const DropdownButton = ({ children, ...props }: DropdownButtonProps) => (
   <Menu.Item>
     {({ active }) => (
       <button
-        className={`w-full text-left block px-4 py-2 text-sm leading-5 text-gray-700 ${
+        className={`w-full text-left block px-4 py-2 text-base leading-5 text-gray-700 ${
           active ? 'bg-gray-100' : ''
         } focus:outline-none transition duration-150 ease-in-out`}
         {...props}
